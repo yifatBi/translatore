86f320d67bfdc5dbe293564569f3b77a7332128f
Yifat Biezuner <yifatbij@gmail.com>
2018-05-04 12:03:01 +0300

    controller

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(
+    private http: HttpClient) {}
   title = 'app';
+  translateResult;
+  submitFunction(text) {
+    this.http.get('https://conduit.productionready.io/api/profiles/eric').subscribe( data => {
+      alert(JSON.stringify(data));
+      this.translateResult = JSON.stringify(data);
+    });
+
+
+  }
 }

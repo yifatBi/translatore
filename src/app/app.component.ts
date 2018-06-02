3ca51d854eda851068edb4a06672c66bfee80229
Yifat Biezuner <yifatbij@gmail.com>
2018-06-02 14:16:27 +0300

    new UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {logging} from 'selenium-webdriver';
 
 
 @Component({
@@ -10,11 +11,13 @@ import {HttpClient} from '@angular/common/http';
 export class AppComponent {
   constructor(
     private http: HttpClient) {}
-  title = 'Auto Programming Translator';
   translateResult;
+  text = '';
+  code = '';
   loader = false;
   submitFunction(text) {
     this.loader = true;
+    console.log(text);
     this.http.get('https://conduit.productionready.io/api/profiles/eric').subscribe( data => {
       this.loader = false;
       // alert(JSON.stringify(data));
